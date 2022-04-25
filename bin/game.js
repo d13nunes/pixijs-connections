fdee479ec897670becd130d52d83dc5bf45da2a2
Diogo Nunes <dnunes4dev@gmail.com>
2022-04-25 13:44:42 +0100

    add multiple circles. each circle has connection line

--- bin/game.js
+++ bin/game.js
@@ -1,64 +1,116 @@
 const Application = PIXI.Application;
 
 const app = new Application({
-    width: 500,
     height: 500,
-    transparent: false,
+    transparency: false,
+    width: 500,
     antialias: true
 });
 
-app.renderer.backgroundColor = 0x23395D;
+app.renderer.backgroundColor = 0x000000;
+const size = {
+    width: 500,
+    height: 500,
+}
 
-app.renderer.resize(500, 500);
+app.renderer.resize(size.width, size.height);
 
 app.renderer.view.style.position = 'absolute';
 
 document.body.appendChild(app.view);
 
+let dots = []
 
-let redCircle = new PIXI.Graphics()
-redCircle.beginFill(0xff0000)
-redCircle.lineStyle(0)
-redCircle.drawCircle(0, 0, 25)
-redCircle.endFill()
-let dot = new Dot(redCircle)
-app.stage.addChild(dot.sprite)
-dot.sprite.buttonMode = true
-dot.sprite.interactive = true
+console.log(app.stage);
+app.stage.sortableChildren = true
+function addConnection(dot) {
 
+    let connectionMode = false
+    let connectionLine = new PIXI.Graphics()
+    connectionLine.zIndex = 2
+    connectionLine.zOrder = 2
+    connectionLine.enableSort = true
+    app.stage.addChild(connectionLine)
 
-let connectionMode = false
-let connectionLine = new PIXI.Graphics()
-connectionLine.position.set(dot.sprite.x, dot.sprite.y)
-dot.sprite.x = 250
-dot.sprite.y = 250
-console.log(dot.sprite);
-app.stage.addChild(connectionLine)
-function onDragStart(e) {
-    connectionMode = true
-    console.log("onDragStart", e);
+    function checkConnection(data, otherDots) {
 
+    }
+
+    function onDragStart(e) {
+        connectionMode = true
+        console.log("onDragStart", e);
+    }
+
+    function onDragEnd(e) {
+        connectionMode = false
+        connectionLine.clear()
+    }
+
+    function onDragMove(e) {
+        if (connectionMode === false) return
+        const newPosition = e.data.getLocalPosition(app.stage)
+        connectionLine.clear()
+        connectionLine.lineStyle(2, 0xFFFFFF)
+            .moveTo(dot.sprite.x, dot.sprite.y)
+            .lineTo(newPosition.x, newPosition.y)
+    }
+
+    dot.sprite.on('pointerdown', onDragStart)
+        .on('pointerup', onDragEnd)
+        .on('pointerupoutside', onDragEnd)
+        .on('pointermove', onDragMove);
 }
 
-function onDragEnd(e) {
-    // console.log("onDragEnd", connectionMode, e);
-    connectionMode = false
-    connectionLine.clear()
+function createDot(color, radius) {
+    let redCircle = new PIXI.Graphics();
+    redCircle.beginFill(color);
+    redCircle.lineStyle(0);
+    redCircle.drawCircle(0, 0, radius);
+    redCircle.endFill();
+    let dot = new Dot(redCircle);
+    dot.sprite.zIndex = 1
+    dot.sprite.zOrder = 1
+    dot.sprite.enableSort = true
+    dot.sprite.buttonMode = true;
+    dot.sprite.interactive = true;
+    return dot;
 }
 
-function onDragMove(e) {
-    if (connectionMode === false) return
-    // console.log("onDragMove", connectionMode, e);
-    const newPosition = e.data.getLocalPosition(app.stage)
-    connectionLine.clear()
-    connectionLine.lineStyle(2, 0xFFFFFF)
-        .moveTo(dot.sprite.x, dot.sprite.y)
-        .lineTo(newPosition.x, newPosition.y)
+const dotRadius = 26
+const margin = 16
+const dotsMeta = {
+    1: {
+        color: 0xFF0000,
+        radius: dotRadius,
+        position: { x: dotRadius + margin, y: dotRadius + margin },
+    },
+    2: {
+        color: 0x00FF00,
+        radius: dotRadius,
+        position: { x: size.width - dotRadius - margin, y: dotRadius + margin },
+    },
+    3: {
+        color: 0x0000FF,
+        radius: dotRadius,
+        position: { x: size.width - dotRadius - margin, y: size.height - dotRadius - margin }
+    },
+    4: {
+        color: 0xFF0FF0,
+        radius: dotRadius,
+        position: { x: dotRadius + margin, y: size.height - dotRadius - margin }
+    },
 }
+console.log("cenas");
 
-dot.sprite.on('pointerdown', onDragStart)
-    .on('pointerup', onDragEnd)
-    .on('pointerupoutside', onDragEnd)
-    .on('pointermove', onDragMove);
-
+for (const [key, value] of Object.entries(dotsMeta)) {
+    const dotMeta = value;
+    console.log(dotMeta);
+    var dot = createDot(dotMeta.color, dotMeta.radius);
+    dot.sprite.x = dotMeta.position.x
+    dot.sprite.y = dotMeta.position.y
+    console.log("dot.sprite", dot.sprite);
+    app.stage.addChild(dot.sprite)
+    addConnection(dot)
+    dots.push(dot)
+}
 
